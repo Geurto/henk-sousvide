7f04bd2228417e9868a40664df85fbc69f096d52
peter <peter@ranmarine.io>
2022-09-13 16:59:14 +0200

    generate random temperature every iteration

--- src/henk_sousvide/henk_sousvide/sous_vide.py
+++ src/henk_sousvide/henk_sousvide/sous_vide.py
@@ -2,12 +2,23 @@
 import rclpy
 from rclpy.node import Node
 
+from std_msgs.msg import Float32
+
 import random
 
 
 class SousVide(Node):
     def __init__(self, node_name: str, **kwargs):
         super().__init__(node_name)
+        self.pub_temp = self.create_publisher(Float32, "temperature", 1)
+
+        self.get_logger().info("Started SousVide node.")
+        self.run()
+
+    def run(self):
+        msg = Float32()
+        msg.data = 10 + 60 * random.random()
+        self.pub_temp.publish(msg)
 
 
 def main(args=None):
